affbb1fa615b59f54551a38f3432ca95bdacf12f
JagTheFriend <jagthefriend12@gmail.com>
2021-10-15 19:47:27 +0200

    The website is working

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,26 @@
-console.log(`Total number of threads: ${api.threads}`)
+const apiURL: string = "https://API.jagthefriend.repl.co";
+interface API {
+    output: string
+}
+
+const runBtn: HTMLButtonElement = document.getElementById("runButton") as HTMLButtonElement;
+runBtn.onclick = runCode;
+
+/**
+ * Runs the code provided by the user
+ */
+function runCode(): void {
+    // gets the code written by the user
+    const code: string = (document.getElementById("inputCode") as HTMLTextAreaElement).value;
+    // gets the language selected by the user
+    const language: string = (document.getElementById("languages") as HTMLSelectElement).value;
+
+    // Send a `GET` request to the API
+    fetch(`${apiURL}/compile=${language}_${code}`)
+        .then((data: Response) => data.json())
+        .then(function (data: API){
+            // Get ths output of the response
+            var output = data.output;
+            (document.getElementById("outputCode") as HTMLTextAreaElement).value = `Output: \n${output}`;
+        });
+}
